4299121c940f6fdaeccb242f6ec002c8e81e73c4
Munir Mahmud <munirmahmud6@gmail.com>
2020-11-02 12:10:54 +0600

    Hamberger menu is done

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { FaBars, FaTimes } from 'react-icons/fa';
 import { IconContext } from 'react-icons/lib';
-import { MobileIcon, Nav, NavbarContainer, NavIcon, NavLogo } from './Navbar.elements';
+import { MobileIcon, Nav, NavbarContainer, NavIcon, NavItem, NavLinks, NavLogo, NavMenu } from './Navbar.elements';
 
 const Navbar = () => {
     const [click, setClick] = useState(false);
@@ -20,6 +20,17 @@ const Navbar = () => {
                     <MobileIcon onClick={handleClick}>
                         {click ? <FaTimes /> : <FaBars />}
                     </MobileIcon>
+                    <NavMenu onClick={handleClick} click={click}>
+                        <NavItem>
+                            <NavLinks to="/">Home</NavLinks>
+                        </NavItem>
+                        <NavItem>
+                            <NavLinks to="/service">Service</NavLinks>
+                        </NavItem>
+                        <NavItem>
+                            <NavLinks to="/products">Product</NavLinks>
+                        </NavItem>
+                    </NavMenu>
                 </NavbarContainer>
             </Nav>
         </IconContext.Provider>
