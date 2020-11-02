cd87230475061d02c8ef441a333b8429a695bbc6
Munir Mahmud <munirmahmud6@gmail.com>
2020-11-02 11:19:26 +0600

    Navbar component added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Navbar } from './components';
+import GlobalStyle from './globalStyles';
 
 const App = () => {
     return (
-        <div>
-            React
-        </div>
+        <>
+            <GlobalStyle />
+            <Router>
+                <Navbar />
+            </Router>
+        </>
     )
 }
 
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Nav, NavbarContainer, NavIcon, NavLogo } from './Navbar.elements';
+
+const Navbar = () => {
+    return (
+        <Nav>
+            <NavbarContainer>
+                <NavLogo to="/">
+                    <NavIcon />
+                    ULTRA
+                </NavLogo>
+            </NavbarContainer>
+        </Nav>
+    )
+}
+
+export default Navbar
